8532849dfbb418cb40248498345e9e6b66bca7d6
VNNiH <vinicius.benassuli@belembioenergia.com.br>
2026-03-25 11:32:33 -0300

    Criação de aplicação com Docker e GithubActions

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Teste!"
+    return "Hello World"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
